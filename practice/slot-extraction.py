c2299a4def520813fc29bfafda3582777fdd606d
Tony Montana <ashwin.s1336@gmail.com>
2026-01-14 22:35:43 +0530

    feat : general slot clarification

--- practice/slot-extraction.py
+++ practice/slot-extraction.py
@@ -1,4 +1,5 @@
 from ollama import chat
+import json
 
 
 def extract_stock_slots(user_input):
@@ -24,6 +25,48 @@ def extract_stock_slots(user_input):
 
 
 
+# while required_slots_missing:
+#     ask for ONE missing slot
+#     wait for user response
+#     validate slot
+
+
+def extract_slots(intent, user_input):
+    if intent == "stock":
+        prompt = f"""
+        Extract stock-related fields from the user query.
+        Fields:
+        - symbol
+        - exchange
+
+        If missing or unknown, return null.
+        Respond ONLY in JSON.
+
+        Query: "{user_input}"
+        """
+
+    elif intent == "weather":
+        prompt = f"""
+        Extract weather-related fields.
+        Fields:
+        - location
+
+        Respond ONLY in JSON.
+
+        Query: "{user_input}"
+        """
+
+    else : 
+        return "Sorry, I can help with weather or stock prices. Please clarify."
+
+
+    response = chat(
+        model="llama3.2",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return response["message"]["content"]
+
+
 tests = [
     # "tesla stock prices",
     # "microsoft at NASDAQ",
@@ -32,5 +75,8 @@ tests = [
 
 ]
 
-for t in tests:
-    print(t, ":", extract_stock_slots(t))
+# for t in tests:
+#     print(t, ":", extract_stock_slots(t))
+
+
+print(extract_slots("weather","is it raining in texas?"))
